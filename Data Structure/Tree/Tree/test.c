1835eb3f67e8cf7641f4a87d568137a1049a732c
proud-even-more-than-oreo <19137037322@163.com>
2026-05-26 00:16:47 +0800

    一道oj题目，根据前序得到二叉树，最后再以中序形式输出

--- Data Structure/Tree/Tree/test.c
+++ Data Structure/Tree/Tree/test.c
@@ -20,3 +20,59 @@ int main()
 	BinaryTreeDestory(&root);
 	return 0;
 }
+
+//以下是一道oj题目，根据前序得到二叉树，最后再以中序形式输出
+//#include <math.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//typedef struct BinaryTree
+//{
+//    char data;
+//    struct BinaryTree* left;
+//    struct BinaryTree* right;
+//}BTNode;
+//
+//BTNode* creat_node(char x)
+//{
+//    BTNode* newnode = (BTNode*)malloc(sizeof(BTNode));
+//    if (newnode == NULL)
+//    {
+//        exit(0);
+//    }
+//    newnode->data = x;
+//    newnode->left = newnode->right = NULL;
+//    return newnode;
+//}
+//BTNode* build_tree(char* arr, int* pi)
+//{
+//    if (arr[*pi] == '#')
+//    {
+//        (*pi)++;
+//        return NULL;
+//    }
+//    BTNode* root = creat_node(arr[*pi]);
+//    (*pi)++;
+//    root->left = build_tree(arr, pi);
+//    root->right = build_tree(arr, pi);
+//    return root;
+//}
+//
+//void Inorder(BTNode* root)
+//{
+//    if (root == NULL)
+//    {
+//        return;
+//    }
+//    Inorder(root->left);
+//    printf("%c ", root->data);
+//    Inorder(root->right);
+//}
+//
+//int main()
+//{
+//    char arr[100];
+//    scanf("%s", arr);
+//    int i = 0;
+//    BTNode* root = build_tree(arr, &i);
+//    Inorder(root);
+//}
